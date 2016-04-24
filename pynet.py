109e03dd9ab8f267974f99889e43918c51f28a8e
Free Debreuil <freedebreuil@gmail.com>
2016-04-24 22:31:10 +0900

    conv net visualization

--- pynet.py
+++ pynet.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# import glumpy
 
 np.random.seed(0)
 
@@ -33,23 +32,21 @@ val4 = np.exp(val3) / softmaxDivide4
 
 # layer 4 gradient
 gradVal4 = np.ones(val4.shape)
-
-# print gradVal4
-# print np.exp(val4) / softmaxDivide4
-
 gradVal4 *= np.exp(val4) / softmaxDivide4
 
 # layer 3 gradient
 gradVal3 = gradVal4 / std3
 
+# layer 2 gradient
+# gradWeight2 = gradVal3
+gradWeight2 = np.random.rand(32, 32) / 3
+# gradWeight2 = np.zeros(gradVal3.shape)
+# for x in range(weight2.shape[0]):
+#     for y in range(weight2.shape[1]):
+#         gradWeight2[x, y] =
+#         # gradWeight2 = weight2 * gradVal3
 
-# window = glumpy.Window(512, 512)
-# im = glumpy.Image(z.astype(np.float32), cmap=glumpy.colormap.Grey)
-
-# @window.event
-# def on_draw():
-#     im.blit(0, 0, window.width, window.height)
-# window.mainloop()
+print gradWeight2
 
 def colorize(val):
     ret = np.zeros((val.shape[0], val.shape[1], 3))
@@ -59,49 +56,20 @@ def colorize(val):
             ret[x, y] = (max(v, 0), 0, max(-v, 0))
     return ret
 
-def colorizeWeight(val):
+def colorizeWeight(val, grad):
     ret = np.zeros((val.shape[0] * 4, val.shape[1] * 4, 3))
     for x in range(val.shape[0]):
         for y in range(val.shape[1]):
             for subx in range(3):
                 for suby in range(3):
                     v = val[x, y, subx, suby]
-                    ret[x * 4 + subx, y * 4 + suby] = (max(v, 0), 0, max(-v, 0))
+                    g = grad[x, y]
+                    ret[x * 4 + subx, y * 4 + suby] = (max(v, 0), abs(g), max(-v, 0))
     return ret
-
-# display = np.zeros((val2.shape[0], val2.shape[1], 3))
-# for x in range(display.shape[0]):
-#     for y in range(display.shape[1]):
-#         display[x, y] = colorize(val2[x, y])
-
-print(val2)
-
-display = colorize(val2)
-
-# colorizeV = np.vectorize(colorize)
-#
-# print colorizeV(val2)
-# # print [colorize(y) for y in val2]
-#
-# display = colorizeV(val2) #np.vectorize(weight2.reshape(-1, 3, 3))
-# for i, d in np.ndenumerate(display):
-#     display[i] = (1, 1, 0)
-
-# print(display)
 
 # Plot the grid
 fig, ax = plt.subplots(1, 2)
 ax[0].imshow(colorize(val2), interpolation='none')
-ax[1].imshow(colorizeWeight(weight2), interpolation='none')
+ax[1].imshow(colorizeWeight(weight2, gradWeight2), interpolation='none')
 plt.gray()
 plt.show()
-
-# print gradVal3
-
-# print val2
-# print val3
-# print val4
-# print np.sum(val4)
-# print(gradVal4)
-
-# print gradVal4
